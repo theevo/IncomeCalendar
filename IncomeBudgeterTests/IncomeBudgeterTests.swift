c6fffcf3aac73e6609b9ac1828ab630a55013633
Theo Vora <theo.vora@gmail.com>
2025-12-07 16:24:17 -0600

    setup unit tests, disable UI tests

--- IncomeBudgeterTests/IncomeBudgeterTests.swift
+++ IncomeBudgeterTests/IncomeBudgeterTests.swift
@@ -10,8 +10,8 @@ import Testing
 
 struct IncomeBudgeterTests {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    @Test func zero() async throws {
+        Issue.record("Test zero success!")
     }
 
 }
