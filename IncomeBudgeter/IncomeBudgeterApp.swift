70df48fe5b19413a04a76f791e5877e10fb1eafd
Theo Vora <theo.vora@gmail.com>
2025-12-07 15:39:31 -0600

    Initial Commit

---
+++ IncomeBudgeter/IncomeBudgeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeBudgeterApp.swift
+//  IncomeBudgeter
+//
+//  Created by Tana Vora on 12/7/25.
+//
+
+import SwiftUI
+
+@main
+struct IncomeBudgeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IncomeBudgeterTests/IncomeBudgeterTests.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeBudgeterTests.swift
+//  IncomeBudgeterTests
+//
+//  Created by Tana Vora on 12/7/25.
+//
+
+import Testing
+@testable import IncomeBudgeter
+
+struct IncomeBudgeterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
